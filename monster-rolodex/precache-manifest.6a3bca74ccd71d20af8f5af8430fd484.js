129825d7642465389696cd6e98b9d4c79fcf7436
MingLiangDai <mingliang0921@gmail.com>
2020-04-29 17:36:03 -0400

    Initial Commit

---
+++ monster-rolodex/precache-manifest.6a3bca74ccd71d20af8f5af8430fd484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744777f524099f7dd46c6ead67a5db17",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b5507da49caa57c2c326",
+    "url": "/monster-rolodex/static/css/main.04658aef.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monster-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5507da49caa57c2c326",
+    "url": "/monster-rolodex/static/js/main.70cfe62d.chunk.js"
+  },
+  {
+    "revision": "1c79305e130eba318f28",
+    "url": "/monster-rolodex/static/js/runtime-main.5ec4cad5.js"
+  }
+]);
